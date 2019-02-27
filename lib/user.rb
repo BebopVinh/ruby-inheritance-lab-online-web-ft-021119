4652f3254bb12c9881d261caf0f38fb25e40db88
Vinh Do <vinhdo07@gmail.com>
2019-02-27 22:44:20 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User
   attr_accessor :first_name, :last_name
 
-  def initialize(first, last)
-    @first_name, @last_name = first, last
+  def initialize
+    @first_name, @last_name = nil, nil
   end
 end
